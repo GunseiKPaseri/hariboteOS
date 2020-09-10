472435902cbeadee344139f632a78194217310dc
GunseiKPaseri <GunseiKPaseri@gmail.com>
2020-09-10 17:11:50 +0900

    [4] usablize font and show 'A'

--- bootpack.c
+++ bootpack.c
@@ -40,6 +40,29 @@ struct BOOTINFO {
     char *vram;
 };
 
+static char font_A[16] = {
+    0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
+    0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00
+};
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
+{
+	int i;
+	char *p, d;
+	for (i = 0; i < 16; i++) {
+		p = vram + (y + i) * xsize + x;
+		d = font[i];
+		if ((d & 0x80) != 0) { p[0] = c; }
+		if ((d & 0x40) != 0) { p[1] = c; }
+		if ((d & 0x20) != 0) { p[2] = c; }
+		if ((d & 0x10) != 0) { p[3] = c; }
+		if ((d & 0x08) != 0) { p[4] = c; }
+		if ((d & 0x04) != 0) { p[5] = c; }
+		if ((d & 0x02) != 0) { p[6] = c; }
+		if ((d & 0x01) != 0) { p[7] = c; }
+	}
+	return;
+}
+
 void HariMain(void)
 {
 	char *vram;
@@ -70,6 +93,8 @@ void HariMain(void)
 	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
 	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
 	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+	
+	putfont8(vram, xsize, 5, 5, COL8_FFFFFF, font_A);
 
 	for (;;) {
 		io_hlt();
