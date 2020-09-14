6f0bc79314a75eabd50b47aa9c0d7f5eba07a95e
GunseiKPaseri <GunseiKPaseri@gmail.com>
2020-09-14 23:44:38 +0900

    align position

--- bootpack.c
+++ bootpack.c
@@ -30,7 +30,7 @@ void HariMain(void)
 	init_mouse_cursor8(mcursor, COL8_008484);
 	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 	
-	sprintf(s, "(%d, %d)", mx, my);
+	sprintf(s, "(%3d, %3d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 	
 	enable_mouse(&mdec);
